1cb7e37c18acdf923bb9a95e537dd8423fb5e86d
Jonahboe <jonahwb@byui.edu>
2022-04-28 10:43:06 -0600

    Added mechanism for Plurality voting.

--- voting.py
+++ voting.py
@@ -1,3 +1,4 @@
+import copy
 import operator
 
 
@@ -34,6 +35,27 @@ class Plurality:
     def __init__(self):
         result = None
 
+    def process(self, agents):
+        rankings = dict()
+        for vote in agents[0].votes:
+            rankings[vote] = 0
+        for agent in agents:
+            rankings[agent.votes[0]] = rankings.get(agent.votes[0], 0) + 1
+        rankings = dict(sorted(rankings.items(), key=operator.itemgetter(1), reverse=True))
+        print("Rankings: " + str(rankings))
+        places = list(rankings.values())
+        ranks = list(rankings.keys())
+        if places[0] == places[1]:
+            for agent in agents:
+                if ranks[-1] in agent.votes:
+                    agent.votes.remove(ranks[-1])
+                else:
+                    agent.votes.remove(agent.votes[-1])
+                print(agent.votes)
+            return self.process(agents)
+        else:
+            return ranks[0]
+
 
 # Algorythm for Veto voting
 class Veto:
@@ -50,8 +72,11 @@ class ag:
 if __name__ == '__main__':
     agents = [ag(id=1, votes=[2, 3, 5, 4]),
               ag(id=2, votes=[5, 4, 3, 1]),
-              ag(id=3, votes=[4, 5, 1, 2]),
+              ag(id=3, votes=[5, 4, 1, 2]),
               ag(id=4, votes=[3, 2, 5, 1]),
               ag(id=5, votes=[2, 3, 1, 4])]
     borda = Borda()
-    print(borda.process(agents))
+    print("Borda winner: " + str(borda.process(copy.deepcopy(agents))) + "\n")
+
+    plurality = Plurality()
+    print("Plurality winner: " + str(plurality.process(copy.deepcopy(agents))) + "\n")
