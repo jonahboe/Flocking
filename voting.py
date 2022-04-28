e9f3db6bd1d59917d8a20523c3339acd78edcb09
Jonahboe <jonahwb@byui.edu>
2022-04-28 10:56:33 -0600

    Added mechanism for Veto voting.

--- voting.py
+++ voting.py
@@ -49,18 +49,35 @@ class Plurality:
             for agent in agents:
                 if ranks[-1] in agent.votes:
                     agent.votes.remove(ranks[-1])
-                else:
-                    agent.votes.remove(agent.votes[-1])
                 print(agent.votes)
             return self.process(agents)
         else:
             return ranks[0]
 
 
-# Algorythm for Veto voting
+# Algorythm for Veto with Elimination voting
 class Veto:
     def __init__(self):
         result = None
+
+    def process(self, agents):
+        rankings = dict()
+        for vote in agents[0].votes:
+            rankings[vote] = 0
+        for agent in agents:
+            rankings[agent.votes[-1]] = rankings.get(agent.votes[-1], 0) + 1
+        rankings = dict(sorted(rankings.items(), key=operator.itemgetter(1)))
+        print("Rankings: " + str(rankings))
+        places = list(rankings.values())
+        ranks = list(rankings.keys())
+        if places[0] == places[1]:
+            for agent in agents:
+                if ranks[-1] in agent.votes:
+                    agent.votes.remove(ranks[-1])
+                print(agent.votes)
+            return self.process(agents)
+        else:
+            return ranks[0]
 
 
 class ag:
@@ -75,8 +92,11 @@ if __name__ == '__main__':
               ag(id=3, votes=[5, 4, 1, 2]),
               ag(id=4, votes=[3, 2, 5, 1]),
               ag(id=5, votes=[2, 3, 1, 4])]
-    borda = Borda()
-    print("Borda winner: " + str(borda.process(copy.deepcopy(agents))) + "\n")
+    voting = Borda()
+    print("Borda winner: " + str(voting.process(copy.deepcopy(agents))) + "\n")
 
-    plurality = Plurality()
-    print("Plurality winner: " + str(plurality.process(copy.deepcopy(agents))) + "\n")
+    voting = Plurality()
+    print("Plurality winner: " + str(voting.process(copy.deepcopy(agents))) + "\n")
+
+    voting = Veto()
+    print("Veto winner: " + str(voting.process(copy.deepcopy(agents))) + "\n")
